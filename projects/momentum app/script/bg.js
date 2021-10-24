512b58db59685fb0ee186e3998bc303d33de5f94
MaoMD <maodayuja@gmail.com>
2021-10-24 22:30:05 +0800

    modified bg function

---
+++ projects/momentum app/script/bg.js
@@ -0,0 +1,19 @@
+// function for background changing according to time
+function setBg() {
+  let today = new Date(),
+      hour = today.getHours();
+
+      // condition
+  if (hour < 12) {
+    document.body.style.backgroundImage = "url('images/image1.jpg')";
+    document.body.style.backgroundSize = "cover";
+  } else if (hour < 18) {
+    document.body.style.backgroundImage = "url('images/image2.jpg')";
+    document.body.style.backgroundSize = "cover";
+  } else {
+    document.body.style.backgroundImage = "url('images/image3.jpg')";
+    document.body.style.backgroundSize = "cover";
+  }
+}
+
+setBg();
--- projects/product landing page/script/script.js
+++ projects/product landing page/script/script.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName('navbar')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const button = document.getElementById ('submit') 
+
+function message () {
+  console.log('this button has been clicked');
+}
+
+button.addEventListener('click', message) {
+  alert(message);
+}
